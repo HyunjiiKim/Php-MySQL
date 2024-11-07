6853981cff730125e6d4373c86256210d0a7cce2
hyunjiikim <hjkim96727@gmail.com>
2024-11-07 07:42:46 +0100

    Create Homepage

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+    $pages=[
+        'Ch4' => 'Objects & Classes',
+        'section_b/ch5' => 'Built-In Functions',
+        'section_b/ch6' => 'Getting Data From Browsers',
+        'section_b/ch7' => 'Images & Files',
+        'section_b/ch8' => 'Dates & Times',
+        'section_b/ch9' => 'Cookies & Sessions',
+        'section_b/ch10' => 'Error Handling',
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <h2>Index</h2>
+            <ul>
+                <?php
+                foreach($pages as $folder => $label){?>
+                    <a src='<?=$folder?>/hompage.php'><li><?=$label?></li><a>
+                <?php }; ?>
+            </ul>
+    </main>
+    <footer></footer>
+</body>
+</html>
