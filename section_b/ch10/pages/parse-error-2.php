13d4ae9aab5ae0b01495799aa649401d2ca9c972
hyunjiikim <hjkim96727@gmail.com>
2024-11-16 18:05:27 +0100

    parse errors

---
+++ section_b/ch10/pages/parse-error-2.php
@@ -0,0 +1,14 @@
+<?php
+$username='hyunjikim'
+$order=['coconut oil','peanut butter','oat milk',];
+?>
+
+<h1>My Cart<h1>
+<div>
+    Name: <?=$username?>
+</div>
+<div>
+    <?php foreach($order as $item) {?>
+    <li><?= $item ?></li> 
+    <?php } ?>
+</div>
